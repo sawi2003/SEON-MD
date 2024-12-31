fad1c326ffd9c5d2ba1f87ad228570350ec87e60
sawi2003 <96415110+sawi2003@users.noreply.github.com>
2024-12-31 19:10:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put the session-id here' : process.env.SESSION_ID,
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Put your github username': process.env.GITHUB_USERNAME,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=Tc8wCDzQ#JVRWLoTZ9M8j80U2PnN4AhajM_4tCcORz0JPuWXmmG4' : process.env.SESSION_ID,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'sawi2003': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
 };
